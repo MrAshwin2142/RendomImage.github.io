40b730f525a44fae287374f840b64c96c6384eca
MrAshwin2142 <89156541+MrAshwin2142@users.noreply.github.com>
2022-06-30 09:20:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+document.querySelector("#input").addEventListener("keydown", (event) => {
+  if (event.key == "Enter")
+    apiRequest();
+});
+
+document.querySelector("#search").addEventListener("click", () => {
+    apiRequest();
+});
+
+apiRequest = () => {
+
+  document.querySelector("#grid").textContent = "";
+
+  const url = 'https://api.unsplash.com/search/photos?query='+input.value+'&per_page=30&client_id=y8vhQOGzqf2cn3wGVKHF_4ki2nmdVoCvC7NDQs7Le6A';
+
+  fetch(url)
+
+  .then(response => {
+    if (!response.ok) throw Error(response.statusText);
+      return response.json();
+   })
+
+   .then(data => {
+      loadImages(data);
+   })
+
+   .catch(error => console.log(error));   
+}
+
+loadImages = (data) => {
+  for(let i = 0;i < data.results.length;i++){
+    let image = document.createElement("div");
+    image.className = "img";
+    image.style.backgroundImage = "url("+data.results[i].urls.raw + "&w=1366&h=768" +")";
+    image.addEventListener("dblclick", function(){
+      window.open(data.results[i].links.download, '_blank');
+    })
+    document.querySelector("#grid").appendChild(image);
+  }
+}
